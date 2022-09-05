7dd23a02db6ba2b8bb2e6af62a54e8a5d8b7110c
young <willy4202@naver.com>
2022-09-05 17:35:23 +0900

    add: vue3 라이브러리 세팅 완료

--- src/main.js
+++ src/main.js
@@ -3,25 +3,27 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 
-import "bootstrap";
+import "bootstrap-vue-3";
 import JQuery from "jquery";
 window.$ = JQuery;
 
 import "bootstrap/scss/bootstrap.scss";
+import "bootstrap-vue-3";
 import "hooper/dist/hooper.css";
+import "vue3-carousel";
 import "vue-scroll-picker/lib/style.css";
 
 import VueCountdown from "@chenfengyuan/vue-countdown";
-import VueGtag from "vue-gtag";
+import VueGtag from "vue-gtag-next";
 import VueUploadComponent from "vue-upload-component";
-import VueYoutube from "vue3-youtube";
-import VueScrollPicker from "vue-scroll-picker";
+import YouTube from "vue3-youtube";
+import VueScrollPicker from "vue3-scroll-picker";
 
 createApp(App)
   .component("file-upload", VueUploadComponent)
   .component(VueCountdown.name, VueCountdown)
+  .component("YouTube", YouTube)
   .use(VueScrollPicker)
-  .use(VueYoutube)
   .use(VueGtag, {
     id: "UA-159915865-1",
     router,
