2cf320debc3a445f56430889b1e20974be8988cf
young <willy4202@naver.com>
2022-09-06 17:10:58 +0900

    add : Header comp, I18nVue와 병합

--- src/components/I18n.vue
+++ src/components/I18n.vue
@@ -1,9 +1,47 @@
 <template>
+  <nav class="container mb-4 mt-4">
+    <div class="row">
+      <div class="col-9"></div>
+      <div class="col-3">
+        <select v-model="$i18n.locale">
+          <option
+            v-for="locale in $i18n.availableLocales"
+            :key="locale"
+            :value="locale"
+          >
+            {{ locale }}
+          </option>
+        </select>
+      </div>
+    </div>
+  </nav>
   <h1>{{ $t("title") }}</h1>
   <p>{{ $t("hello") }}</p>
+  {{ lang }}
 </template>
 <script>
 export default {
   name: "I18n",
+  data() {
+    return {
+      lang: "",
+    };
+  },
+  created() {
+    console.log(this.$i18n.locale);
+  },
+  methods: {
+    /**  쿠키 확인 후, 있으면 쿠키를 바탕으로 초기 언어를 세팅해주는 함수 */
+    checkCookie() {},
+
+    /** select에서 언어를 선택하면, 해당 언어를 key value로 가지는 cookie를 생성해주는 함수 */
+    setCookie(key, value, expired) {},
+
+    /** 브라우저상에서 유저가 선호하는 언어가,
+     * i18n 프로젝트 내에서 지원이 가능한 언어라면
+     * 해당 언어를 기본 세팅으로 만들어주는 함수
+     */
+    checkPreferLang() {},
+  },
 };
 </script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,21 +1,21 @@
 <template>
   <div class="home">
-    <Header />
     <img alt="Vue logo" src="../assets/logo.png" />
     <I18n />
   </div>
 </template>
 
 <script>
-import Header from "@/components/Header.vue";
 import I18n from "@/components/I18n.vue";
 
 export default {
-  name: "HomeView",
+  name: "",
   components: {
-    Header,
     I18n,
   },
   created() {},
+  data() {
+    return {};
+  },
 };
 </script>
