4e747523ed3fc46e13ab464c146482b5e23e5802
Young <willy4202@naver.com>
2022-09-09 01:35:09 +0900

    add: 변수명 리팩토링

--- src/views/I18nView.vue
+++ src/views/I18nView.vue
@@ -23,29 +23,29 @@ export default {
     return {};
   },
   created() {
-    // 언어를 1순위 쿠키, 2순위 유저가 선호하는 언어,
-    // 3순위 basic 언어를 메인 언어로 설정한다.
-    // setLocale?
-    let browserLang = navigator.language.split("-")[0];
-
-    this.$i18n.locale =
-      this.$cookies.get("lang") ||
-      this.$i18n.availableLocales.includes(browserLang)
-        ? browserLang
-        : this.$i18n.fallbackLocale;
-
-    // this.$i18n.locale = this.$cookies.get("lang") || this.getLangInBroswer();
-
-    // let browserLang = navigator.language.split("-")[0];
-    // this.$i18n.availableLocales.includes(browserLang)
-    //   ? browserLang
-    //   : this.$i18n.fallbackLocale;
+    this.setLocale();
   },
   updated() {
     this.selectLocale();
   },
 
   methods: {
+    setLocale() {
+      this.$i18n.locale =
+        this.getLoclaeFromCookie() ||
+        this.getLocaleFromBroswer() ||
+        this.$i18n.fallbackLocale;
+    },
+
+    getLoclaeFromCookie() {
+      this.$cookies.get("lang");
+    },
+
+    getLocaleFromBroswer() {
+      const browserLang = navigator.language.split("-")[0];
+      this.$i18n.availableLocales.includes(browserLang) ? browserLang : null;
+    },
+
     selectLocale() {
       this.$cookies.set("lang", this.$i18n.locale);
     },
