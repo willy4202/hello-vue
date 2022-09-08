fa8301a5c5839890dfecf2fba68061668b009be1
young <willy4202@naver.com>
2022-09-08 09:58:22 +0900

    modify: 변수명 및 컴포넌트 파일 수정

--- src/views/I18nView.vue
+++ src/views/I18nView.vue
@@ -1,20 +1,26 @@
 <template>
-  <SelectLang />
+  <nav class="container mb-4 mt-4">
+    <div class="col-3">
+      <select v-model="$i18n.locale">
+        <option
+          v-for="locale in $i18n.availableLocales"
+          :key="locale"
+          :value="locale"
+        >
+          {{ locale }}
+        </option>
+      </select>
+    </div>
+  </nav>
   <h1>{{ $t("title") }}</h1>
   <p>{{ $t("hello") }}</p>
   <p>{{ $t("message.bye", { msg: "goodbye" }) }}</p>
   <p>{{ $t("message.go", ["lets'go"]) }}</p>
-  <p>email : {{ $t("address", { account: "willy", domain: "naver.com" }) }}</p>
 </template>
 
 <script>
-import SelectLang from "@/components/SelectLang.vue";
-
 export default {
   name: "I18n",
-  components: {
-    SelectLang,
-  },
   data() {
     return {};
   },
@@ -26,23 +32,21 @@ export default {
   },
 
   methods: {
-    /**  쿠키 확인 후, 있으면 쿠키를 바탕으로 초기 언어를 세팅해주는 함수 */
     getLang() {
       this.$i18n.locale = this.$cookies.get("lang")
         ? this.$cookies.get("lang")
         : getLangInBroswer();
     },
-    /** 
-      유저가 선호하는 언어를 찾아서, 프로젝트 내에서 지원이 가능한 언어라면
-      선호 언어를 기본 언어로 만들어주는 함수
-     */
+
     getLangInBroswer() {
       let broswerLang = navigator.language.split("-")[0];
       if (this.$i18n.availableLocales.includes(broswerLang)) {
         return broswerLang;
+      } else {
+        return this.$i18n.locale;
       }
     },
-    /** select에서 언어를 선택하면, 해당 언어를 key value로 가지는 cookie를 생성해주는 함수 */
+
     setLang() {
       this.$cookies.set("lang", this.$i18n.locale);
     },
