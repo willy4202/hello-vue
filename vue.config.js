8d85a7174f2ce0a984e0bf843a3cd1fb7e5d06f8
young <willy4202@naver.com>
2022-09-21 18:31:35 +0900

    add: mini css plugin, rtl-plugin

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,7 @@
-const { defineConfig } = require("@vue/cli-service");
-module.exports = defineConfig({
-  transpileDependencies: true,
-});
+const WebpackRtlPlugin = require("webpack-rtl-plugin");
+
+module.exports = {
+  css: {
+    extract: {},
+  },
+};
