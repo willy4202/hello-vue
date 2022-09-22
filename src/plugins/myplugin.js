eb044bb19669e5db76c0b9bfe864a0036b38f7c4
young <willy4202@naver.com>
2022-09-22 15:45:20 +0900

    RTLCSS 설치 및 설정

---
+++ src/plugins/myplugin.js
@@ -0,0 +1,9 @@
+class MyPlugin {
+  apply(compiler) {
+    compiler.hooks.done.tap("My Plugin", (stats) => {
+      console.log("MyPlugin: done");
+    });
+  }
+}
+
+module.exports = MyPlugin;
--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,29 @@
+const { defineConfig } = require("@vue/cli-service");
 const WebpackRtlPlugin = require("webpack-rtl-plugin");
 
-module.exports = {
+module.exports = defineConfig({
   css: {
-    extract: {},
+    extract: {
+      filename: "css/[name].css",
+      chunkFilename: "css/[name].[hash].css",
+    },
   },
-};
+  configureWebpack: {
+    plugins: [
+      new WebpackRtlPlugin({
+        autoRename: true,
+        stringMap: [
+          {
+            name: "ltr-rtl",
+            search: "ltr",
+            replace: "rtl",
+            options: {
+              scope: "*",
+              ignoreCase: false,
+            },
+          },
+        ],
+      }),
+    ],
+  },
+});
