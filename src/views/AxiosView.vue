af4d84336ba62e975972e56ae5a19b9ace256467
young <willy4202@naver.com>
2022-10-11 11:58:20 +0900

    modify: Btn 렌더링 관련 데이터 리팩토링

--- src/views/AxiosView.vue
+++ src/views/AxiosView.vue
@@ -2,17 +2,18 @@
   <h2>now {{ currentMode }} mode</h2>
   <h3>server CORS setting is 'true'</h3>
   <h3>without Proxy</h3>
+
   <article>
-    <section v-for="data in btnData" :key="data">
+    <section v-for="data in btnHandler" :key="data">
       <h4>{{ data.name }} Data</h4>
-      <button @click="data.method">{{ data.name }}</button>
+      <button @click="btnEventHandler(data.name)">{{ data.name }}</button>
     </section>
   </article>
   <h3>with Proxy</h3>
   <article>
-    <section v-for="data in useProxyBtnData" :key="data">
+    <section v-for="data in btnHandler" :key="data">
       <h4>{{ data.name }}Data</h4>
-      <button @click="data.method">{{ data.name }}</button>
+      <button @click="proxyBtnEvnetHandler(data.name)">{{ data.name }}</button>
     </section>
   </article>
 </template>
@@ -27,138 +28,39 @@ export default {
     return {
       currentMode:
         process.env.NODE_ENV === "development" ? "dev" : "production",
-      btnData: [
+
+      btnHandler: [
         {
           name: "get",
-          method: () => {
-            this.getData();
-          },
         },
         {
           name: "post",
-          method: () => {
-            this.postData();
-          },
         },
         {
           name: "put",
-          method: () => {
-            this.putData();
-          },
         },
         {
           name: "patch",
-          method: () => {
-            this.patchData();
-          },
         },
         {
           name: "delete",
-          method: () => {
-            this.deleteData();
-          },
-        },
-      ],
-      useProxyBtnData: [
-        {
-          name: "get",
-          method: () => {
-            this.proxyGetData();
-          },
-        },
-        {
-          name: "post",
-          method: () => {
-            this.proxyPostData();
-          },
-        },
-        {
-          name: "put",
-          method: () => {
-            this.proxyPutData();
-          },
-        },
-        {
-          name: "patch",
-          method: () => {
-            this.proxyPatchData();
-          },
-        },
-        {
-          name: "delete",
-          method: () => {
-            this.proxyDeleteData();
-          },
         },
       ],
     };
   },
 
   methods: {
-    getData() {
+    btnEventHandler(name) {
       return instance({
         url: URL,
-        method: "get",
+        method: name,
       });
     },
 
-    postData() {
-      return instance({
-        url: URL,
-        method: "post",
-      });
-    },
-
-    putData() {
-      return instance({
-        url: URL,
-        method: "put",
-      });
-    },
-
-    patchData() {
-      return instance({
-        url: URL,
-        method: "patch",
-      });
-    },
-
-    deleteData() {
-      return instance({
-        url: URL,
-        method: "delete",
-      });
-    },
-
-    proxyGetData() {
+    proxyBtnEvnetHandler(name) {
       return proxyInstance({
         url: URL,
-        method: "get",
-      });
-    },
-
-    proxyPostData() {
-      return proxyInstance({
-        url: URL,
-        method: "post",
-      });
-    },
-    proxyPutData() {
-      return proxyInstance({
-        url: URL,
-        method: "put",
-      });
-    },
-    proxyPatchData() {
-      return proxyInstance({
-        url: URL,
-        method: "patch",
-      });
-    },
-    proxyDeleteData() {
-      return proxyInstance({
-        url: URL,
-        method: "delete",
+        method: name,
       });
     },
   },
