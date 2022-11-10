739913faf976070ca8c7982c48131f80089e0506
young <willy4202@naver.com>
2022-11-10 09:24:34 +0900

    modify: locale font

--- src/views/WebFontView.vue
+++ src/views/WebFontView.vue
@@ -32,51 +32,7 @@ export default {
   name: "font",
   components: { LangSelector },
   data() {
-    return {
-      documentLang: "en-US",
-    };
-  },
-
-  created() {
-    // this.resolveFontLink("Noto+Sans");
-  },
-
-  updated() {
-    // this.changeFont();
-  },
-
-  methods: {
-    /** locale 코드에 맞춰 폰트 설정 */
-    // changeFont() {
-    //   const locale = this.$i18n.locale;
-    //   switch (locale) {
-    //     case "en-US":
-    //       this.resolveFontLink("Noto+Sans");
-    //       break;
-    //     case "ar-AE":
-    //       this.resolveFontLink("Noto+Sans+Arabic");
-    //       break;
-    //     default:
-    //       return;
-    //   }
-    // },
-    // resolveFontLink(font) {
-    //   this.removePrevFontLink(font);
-    //   this.appendFontLink(font);
-    // },
-    // appendFontLink(font) {
-    //   const head = document.getElementsByTagName("head")[0];
-    //   const link = document.createElement("link");
-    //   link.id = font;
-    //   link.rel = "stylesheet";
-    //   link.type = "text/css";
-    //   link.href = `https://fonts.googleapis.com/css2?family=${font}:wght@300;400;600&display=swap`;
-    //   head.appendChild(link);
-    // },
-    // removePrevFontLink(id) {
-    //   const links = document.querySelectorAll("link[id]");
-    //   Array.from(links).map((link) => (link.id !== id ? link.remove() : null));
-    // },
+    return {};
   },
 };
 </script>
