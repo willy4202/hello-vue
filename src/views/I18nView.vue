2ed76f3c5fd5d4326ff77bf207a9e4cb63c83f14
young <willy4202@naver.com>
2022-11-22 15:03:18 +0900

    add: docker dev, prod set

--- src/views/I18nView.vue
+++ src/views/I18nView.vue
@@ -11,6 +11,9 @@ import LangSelector from '@/components/LangSelector.vue';
 export default {
 	components: { LangSelector },
 	name: 'I18n',
+	created() {
+		console.log(process.env.VUE_APP_PHASE);
+	},
 };
 </script>
 
